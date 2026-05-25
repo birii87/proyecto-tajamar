38f2adf7ba470735e2ef2650ac49d64d5f3ec6b3
Manish Soneji <manish.soneji@hotmail.com>
2026-05-25 21:47:43 +0200

    Se ha añadido una nueva función para formatear los gastos y su test

--- gasto.py
+++ gasto.py
@@ -1,5 +1,20 @@
+from tabulate import tabulate
+
 def crear_gasto(nombre,precio):
     return {
         "nombre": nombre,
         "precio": precio
     }
+
+def formatear_gastos (lista_de_gastos):
+    tabla = []
+
+    for gasto in lista_de_gastos:
+        tabla.append(
+            [
+                gasto["nombre"],
+                gasto["precio"]
+             ]
+        )
+
+    return tabulate(tabla,headers=["Nombre","Precio"])
--- test_gastos.py
+++ test_gastos.py
@@ -1,7 +1,18 @@
-from gasto import crear_gasto
+from gasto import crear_gasto, formatear_gastos
 
 def test_crear_gasto():
     gasto = crear_gasto("Pistachos", 3.49)
 
     assert gasto["nombre"] == "Pistachos"
     assert gasto["precio"] == 3.49
+
+
+def test_formato():
+    gastos = [
+        {"nombre": "Pistachos", "precio": 3.49},
+        {"nombre": "Chocolate", "precio": 1.99}
+    ]
+
+    salida = formatear_gastos(gastos)
+
+    assert "Pistachos" in salida
