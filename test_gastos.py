0b5999d310fd105180947ea38e1b59c5b6277e11
Manish Soneji <manish.soneji@hotmail.com>
2026-05-26 20:44:11 +0200

    Cambios gastos a class objeto

--- test_gastos.py
+++ test_gastos.py
@@ -1,18 +1,25 @@
-from gasto import crear_gasto, formatear_gastos
+from gasto import Gasto, ListaGastos
 
 def test_crear_gasto():
-    gasto = crear_gasto("Pistachos", 3.49)
 
-    assert gasto["nombre"] == "Pistachos"
-    assert gasto["precio"] == 3.49
+    gasto = Gasto("Pistachos", 3.49)
 
+    assert gasto.nombre == "Pistachos"
+    assert gasto.precio == 3.49
+
+def test_añadir_gasto():
+    carrito = ListaGastos()
+
+    gasto = Gasto("Pistachos", 3.49)
+    carrito.crear_gasto(gasto)
+    assert len(carrito.gastos) == 1
 
 def test_formato():
-    gastos = [
-        {"nombre": "Pistachos", "precio": 3.49},
-        {"nombre": "Chocolate", "precio": 1.99}
-    ]
+    
+    carrito = ListaGastos()
+    carrito.crear_gasto(Gasto("Pistachos", 3.49)),
+    carrito.crear_gasto(Gasto("Chocolate", 1.99))
 
-    salida = formatear_gastos(gastos)
+    salida = carrito.formatear_gasto()
 
     assert "Pistachos" in salida
